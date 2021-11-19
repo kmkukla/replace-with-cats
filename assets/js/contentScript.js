175ca7fcdccffb5967db1932b8a65143d81bca87
Konrad Kukla <kondziocard@gmail.com>
2021-11-19 14:06:24 +0100

    Initial commit :octocat:

---
+++ assets/js/contentScript.js
@@ -0,0 +1,12 @@
+console.log(`Content script is running`);
+
+let images = document.getElementsByTagName('img');
+
+for (let i = 0; i < images.length; i++) {
+  chrome.runtime.sendMessage(
+    { msg: 'image', index: i },
+    function ({ data, index }) {
+      images[index].src = data.link;
+    }
+  );
+}
